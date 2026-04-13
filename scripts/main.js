7524cbf8c3d88f9f1aa4d3ecb71f0ff6dcd00567
Dimas <zdm2507@gmail.com>
2026-04-13 15:01:24 +0500

    auth sign up

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,31 @@ toggleBtn.addEventListener('click', () => {
     sidebar.classList.toggle('sidebar--expanded');
     console.log('toggled:', sidebar.classList.contains('sidebar--expanded'));
 });
+// === МОДАЛКА РЕГИСТРАЦИИ ===
+const openModalBtn = document.getElementById("openModal");
+const modal = document.getElementById("modal");
+const closeModalBtn = document.getElementById("closeModal");
+
+// открыть модалку
+if (openModalBtn) {
+    openModalBtn.addEventListener("click", () => {
+        modal.classList.add("active");
+    });
+}
+
+// закрыть по стрелке
+if (closeModalBtn) {
+    closeModalBtn.addEventListener("click", () => {
+        modal.classList.remove("active");
+    });
+}
+
+// закрыть по клику вне окна
+if (modal) {
+    modal.addEventListener("click", (e) => {
+        if (e.target === modal) {
+            modal.classList.remove("active");
+        }
+    });
+}
+
